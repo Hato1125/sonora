512c1a3e207fcd6ffad3df2985359e484b7bc2f6
nolight132 <contact@nolight.dev>
2026-08-03 22:24:34 +0200

    chore: add nix dev shell and gitignore

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "spotty - a minimal GPUI client for the Spotify Web API";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forEachSystem = fn: nixpkgs.lib.genAttrs systems (system: fn nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forEachSystem (
+        pkgs:
+        let
+          runtimeLibraries = with pkgs; [
+            vulkan-loader
+            wayland
+            libxkbcommon
+            libxcb
+            libx11
+            libxcursor
+            libxi
+            fontconfig
+            freetype
+          ];
+        in
+        {
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              rustc
+              rust-analyzer
+              rustfmt
+              sccache
+            ];
+
+            buildInputs = runtimeLibraries;
+
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath runtimeLibraries;
+          };
+        }
+      );
+    };
+}
