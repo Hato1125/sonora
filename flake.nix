47b7551df410d00f0262a8cddb650a8e7c471301
nolight132 <contact@nolight.dev>
2026-08-05 21:36:02 +0200

    build(nix): add package in flake.nix

--- flake.nix
+++ flake.nix
@@ -1,7 +1,9 @@
 {
-  description = "spotty - a minimal GPUI client for the Spotify Web API";
+  description = "spotty - a minimal native Spotify client";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
 
   outputs =
     { nixpkgs, ... }:
@@ -10,10 +12,11 @@
         "x86_64-linux"
         "aarch64-linux"
       ];
+
       forEachSystem = fn: nixpkgs.lib.genAttrs systems (system: fn nixpkgs.legacyPackages.${system});
     in
     {
-      devShells = forEachSystem (
+      packages = forEachSystem (
         pkgs:
         let
           runtimeLibraries = with pkgs; [
@@ -29,6 +32,56 @@
             alsa-lib
           ];
 
+          spotty = pkgs.rustPlatform.buildRustPackage {
+            pname = "spotty";
+            version = "0.1.0";
+
+            src = ./.;
+
+            cargoHash = "sha256-8GIkBbhaZwI21owOybq4cA+qsnT8oqs4Z+z24vvWRHo=";
+
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              mold
+              bintools
+            ];
+
+            buildInputs = runtimeLibraries;
+
+            postFixup = ''
+              patchelf \
+                --add-rpath "${pkgs.lib.makeLibraryPath runtimeLibraries}" \
+                "$out/bin/spotty"
+            '';
+
+            meta = {
+              description = "A minimal native Spotify client built with GPUI";
+              mainProgram = "spotty";
+              platforms = pkgs.lib.platforms.linux;
+            };
+          };
+        in
+        {
+          inherit spotty;
+          default = spotty;
+        }
+      );
+
+      devShells = forEachSystem (
+        pkgs:
+        let
+          runtimeLibraries = with pkgs; [
+            vulkan-loader
+            wayland
+            libxkbcommon
+            libxcb
+            libx11
+            libxcursor
+            libxi
+            fontconfig
+            freetype
+            alsa-lib
+          ];
         in
         {
           default = pkgs.mkShell {
@@ -44,7 +97,6 @@
             buildInputs = runtimeLibraries;
 
             LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath runtimeLibraries;
-
           };
         }
       );
